5e6b8ce33674c1e2ca926ae100d0eab7f98cfae0
Susu <samin.abdallahi@gmail.com>
2026-03-15 16:16:30 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
-  password: "changeme123",
+  password: "Joonz",
   apiUrl: "https://script.google.com/macros/s/AKfycbyf8uTKoaGkp3EYisvLHlTCNNNmBby4FuPkQpdhST7PkFJDlRtkr1sRwywAcUV921HWqQ/exec"
 };
